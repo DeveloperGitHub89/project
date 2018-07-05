e505a7aa88028138fc0bc5d14a669d888c2d10f5
abhi11111 <37204429+abhi11111@users.noreply.github.com>
2018-07-05 09:31:53 +0530

    this is change

--- addEmployee.php
+++ addEmployee.php
@@ -1,33 +1,6 @@
 <?php 
 
+echo "hello";
+$var="hiiiiiiiiii";
 
-try {
-require 'db.php';
-$db=new Database;
-$conn=$db->getConnection();
-$qry="INSERT INTO emp (id,name,salary,doj) 
-    VALUES (:id,:name,:salary,:doj)";
-    // prepare sql and bind parameters
-    
-    $id=htmlspecialchars($_POST["id"]);
-     $name=htmlspecialchars($_POST["name"]);
-      $doj=htmlspecialchars($_POST["doj"]);
-       $salary=htmlspecialchars($_POST["salary"]);
-       $result=$stmt->execute();
-       if ($result) {
-       	$response=array("status"=>1,"status_message"=>"data inserted");
-       }
-       else{
-       	$response=array("status"=>0,"status_message"=>"error in  inserting");
-       }
-       
-} catch (PDOException $e) {
-	
-	if ($e->getCode()==23000) {
-		$response=array("status"=>0,"status_message"=>"duplicate entry for id");
-	}
-	 	
-}
-header('Content-Type: application/json');
-      echo  json_encode($response);	
  ?>
